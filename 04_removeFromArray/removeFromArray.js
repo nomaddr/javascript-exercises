2d4851bfa832144abd20ec290b8234496790786a
Robin WSL <robin.daub1@gmail.com>
2024-10-24 19:38:18 +0200

    finish ex 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,17 +3,18 @@ const removeFromArray = function() {
   if (arguments.length < 2) return arguments[0];
 
   let array = arguments[0]
-  let result = [];
 
   for (let i=1; i < arguments.length; i++) {
     for (let k = 0; k < array.length; k++) {
-      if (array[k] != arguments[i]) {
-        result.push(array[k]);
+      if (array[k] === arguments[i]) {
+        delete array[k];
       }
     }
   }
-  console.log(result);
-  return result;
+  array = array.filter(function(element) { 
+        return element !== undefined;
+        });
+  return array;
 };
 
 // Do not edit below this line
